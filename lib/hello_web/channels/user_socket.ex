5d546e752b98000c7ae87a2c0060d7d4773a9508
Vinod Hegde <vinodhg@gmail.com>
2018-05-02 23:14:24 -0700

    Change to longpoll instead of websocket

--- lib/hello_web/channels/user_socket.ex
+++ lib/hello_web/channels/user_socket.ex
@@ -6,9 +6,9 @@ defmodule HelloWeb.UserSocket do
   channel "room:*", HelloWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  #transport :websocket, Phoenix.Transports.WebSocket
   #  timeout: 45_000
-  #transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
